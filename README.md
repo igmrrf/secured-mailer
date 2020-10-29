ddb6801094ab3873c743981632d812c7a9050459
igmrrf <francis.igbiriki.fi@gmail.com>
2020-10-29 01:09:56 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Simple Secure Nodejs Emailing Setup
+
+1. Node.js/ npm registry
+2. Text Editor: VSCode or your favorite
+3. Gmail Account
+4. Google Cloud Platform Account
+
+Visit your Google cloud Platform account and generate OAuth2 Client API tokens
+
+Give access with the Gmail account and generate refresh token
---
+++ index.js
@@ -0,0 +1,45 @@
+const nodmailer = require('nodemailer');
+const { google } = require('googleapis');
+const OAuth2 = google.auth.OAuth2;
+const config = require('./config.json');
+
+const oauth2Client = new OAuth2(
+  config.CLIENT_ID,
+  config.CLIENT_SECRET,
+  config.REDIRECT_URL
+);
+
+oauth2Client.setCredentials({
+  refresh_token: config.REFRESH_TOKEN,
+});
+
+const accessToken = oauth2Client.getAccessToken();
+
+const smtpTransport = nodmailer.createTransport({
+  service: 'gmail',
+  auth: {
+    type: 'Oauth2',
+    user: config.EMAIL,
+    clientId: config.CLIENT_ID,
+    clientSecret: config.CLIENT_SECRET,
+    refreshToken: config.REFRESH_TOKEN,
+    accessToken: accessToken,
+  },
+  tls: {
+    rejectedUnauthorized: false,
+  },
+});
+function sendMail(to, subject, html) {
+  const mailOptions = {
+    from: config.EMAIL,
+    to,
+    subject,
+    generatedTextFromHTML: true,
+    html,
+  };
+
+  smtpTransport.sendMail(mailOptions, (error, response) => {
+    error ? console.log(error) : console.log(response);
+    smtpTransport.close();
+  });
+}
